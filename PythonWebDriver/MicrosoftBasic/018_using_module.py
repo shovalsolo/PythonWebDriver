0d3a7d0611cf90df5ea19a662fe4cd6ffca4f70e
shovalsolo <shovalso@gmail.com>
2020-06-02 14:01:23 -0400

    Adding comments

--- PythonWebDriver/MicrosoftBasic/018_using_module.py
+++ PythonWebDriver/MicrosoftBasic/018_using_module.py
@@ -1,3 +1,5 @@
+# This is a script that is importing a method from a different file and showing the way to import it
+
 import helpers_018                              #Importing everything under the module
 from helpers_018 import *                       #Importing everything under the module
 from helpers_018 import display                 #Importing a function from a module
